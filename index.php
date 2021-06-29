078c252e1f0c4f9b3e38b09a8ece05e2c8429340
Pankaj Savantre <82807436+pankajps07@users.noreply.github.com>
2021-06-29 21:56:28 +0530

    Update index.php

--- index.php
+++ index.php
@@ -8,6 +8,7 @@ session_start();
     
     
     <title>Kisan</title>
+     <meta name="google-site-verification" content="...">
     <meta name="viewport" content="width=device-width,initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
     <?php include "assets/csslink.php"?>
